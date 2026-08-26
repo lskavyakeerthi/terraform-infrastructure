a7b38a263062a6935148f7ddaea217579864a940
Ubuntu <ubuntu@ip-172-31-12-226.ap-south-1.compute.internal>
2026-08-26 21:20:14 +0000

    Configure S3 remote backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "kavya-terraform-state-123456789012"
+    bucket       = "kavya-terraform-state-1234"
     key          = "terraform-infrastructure/terraform.tfstate"
     region       = "ap-south-1"
     encrypt      = true
